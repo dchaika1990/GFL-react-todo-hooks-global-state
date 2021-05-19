8bd860e187e524e3c480f8b40fa9f0649fb4643b
Dima <dchaika1990@gmail.com>
2021-05-19 09:56:33 +0300

    include reactstrap

--- src/App.js
+++ src/App.js
@@ -1,170 +1,172 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { connect } from 'react-redux';
+import React, {useEffect, useRef, useState} from 'react';
+import {connect} from 'react-redux';
 import List from './components/List';
 import ListItem from './components/ListItem';
-import { bindActionCreators } from 'redux';
+import {bindActionCreators} from 'redux';
+import {Badge, Button, Container, Row, Col, Form, FormGroup, Label, Input} from 'reactstrap'
 
 // import { setTodos, startLoad, endLoad } from './actions';
 import * as actions from './actions';
 
 const App = props => {
-  const [todoList, setTodoList] = useState({
-    inProgress: [],
-    done: [],
-  });
+	const [todoList, setTodoList] = useState({
+		inProgress: [],
+		done: [],
+	});
 
-  const { setTodos, todos, startLoad, fetching, endLoad } = props;
+	const {setTodos, todos, startLoad, fetching, endLoad} = props;
 
-  const addInputRef = useRef();
-  // const todos = useTodos();
+	const addInputRef = useRef();
+	// const todos = useTodos();
 
-  useEffect(() => {
-    (async () => {
-      startLoad();
-      const { in_progress, done } = await fetch('/todos.json').then(res =>
-        res.json()
-      );
-      // setTodoList(state => ({ ...state, inProgress: in_progress, done }));
-      setTodos({
-        in_progress,
-        done,
-      });
-      endLoad();
-    })();
+	useEffect(() => {
+		(async () => {
+			startLoad();
+			const {in_progress, done} = await fetch('/todos.json').then(res =>
+				res.json()
+			);
+			// setTodoList(state => ({ ...state, inProgress: in_progress, done }));
+			setTodos({
+				in_progress,
+				done,
+			});
+			endLoad();
+		})();
 
-    return () => {};
-  }, []);
+		return () => {
+		};
+	}, []);
 
-  // useEffect(() => {
-  //   setTodoList(state => ({
-  //     ...state,
-  //     inProgress: todos.in_progress,
-  //   }));
+	// useEffect(() => {
+	//   setTodoList(state => ({
+	//     ...state,
+	//     inProgress: todos.in_progress,
+	//   }));
 
-  //   setTodoList(state => ({
-  //     ...state,
-  //     done: todos.done,
-  //   }));
-  // }, [todos]);
+	//   setTodoList(state => ({
+	//     ...state,
+	//     done: todos.done,
+	//   }));
+	// }, [todos]);
 
-  const { inProgress, done } = todoList;
+	const {inProgress, done} = todoList;
 
-  const loading = <p>Loading...</p>;
+	const loading = <p>Loading...</p>;
 
-  const renderDoneItem = ({ name, finishedTime }) => (
-    <>
-      <span className='badge'>
-        {new Date(finishedTime).toLocaleTimeString()}
-      </span>
-      {name}
-    </>
-  );
+	const renderDoneItem = ({name, finishedTime}) => (
+		<>
+			<Badge color="secondary" pill>
+				{new Date(finishedTime).toLocaleTimeString()}
+			</Badge>
+			{name}
+		</>
+	);
 
-  const renderInProgressItem = ({ name }) => (
-    <>
-      {name}
-      <button type='button' className='btn btn-primary'>
-        Start
-      </button>
-      <button type='button' className='btn btn-danger'>
-        Del
-      </button>
-    </>
-  );
+	const renderInProgressItem = ({name}) => (
+		<>
+			{name}
+			<Button type={"button"} color="primary">
+				Start
+			</Button>
+			<Button type={"button"} color="danger">
+				Del
+			</Button>
+		</>
+	);
 
-  return (
-    <div className='container'>
-      <h1>Todo React APP</h1>
-      <div className='row'>
-        <div className='col-xs-12'>
-          <form>
-            <div className='form-group'>
-              <label htmlFor='addInput'>New Todo Item: </label>
-              <input
-                ref={addInputRef}
-                id='addInput'
-                type='text'
-                className='form-control'
-                placeholder='New todo name'
-              />
-            </div>
-            <button type='button' className='btn btn-success pull-right'>
-              Add New Item
-            </button>
-          </form>
-        </div>
-      </div>
-      <hr />
-      <div className='row'>
-        <div className='col-xs-12 col-sm-6'>
-          <h3>Todos in progress</h3>
-          {fetching ? (
-            loading
-          ) : (
-            <List>
-              {todos.in_progress.map(item => {
-                const { id } = item;
+	return (
+		<Container>
+			<h1>Todo React APP</h1>
+			<Row>
+				<Col>
+					<Form>
+						<FormGroup>
+							<Label for='addInput'>New Todo Item: </Label>
+							<Input
+								ref={addInputRef}
+								id='addInput'
+								type='text'
+								className='form-control'
+								placeholder='New todo name'
+							/>
+						</FormGroup>
+						<Button type='button' color="success" className='pull-right'>
+							Add New Item
+						</Button>
+					</Form>
+				</Col>
+			</Row>
+			<hr/>
+			<Row>
+				<Col sm="6">
+					<h3>Todos in progress</h3>
+					{fetching ? (
+						loading
+					) : (
+						<List>
+							{todos.in_progress.map(item => {
+								const {id} = item;
 
-                return (
-                  <ListItem
-                    key={id}
-                    item={item}
-                    render={renderInProgressItem}
-                  />
-                );
-              })}
-            </List>
-          )}
-          <p>Things to do: {0}</p>
-        </div>
-        <div className='col-xs-12 col-sm-6'>
-          <h3>Done</h3>
+								return (
+									<ListItem
+										key={id}
+										item={item}
+										render={renderInProgressItem}
+									/>
+								);
+							})}
+						</List>
+					)}
+					<p>Things to do: {0}</p>
+				</Col>
+				<Col sm="6">
+					<h3>Done</h3>
 
-          {fetching ? (
-            loading
-          ) : (
-            <List>
-              {todos.done.map(({ id, ...item }) => (
-                <ListItem key={id} item={item} render={renderDoneItem} />
-              ))}
-            </List>
-          )}
+					{fetching ? (
+						loading
+					) : (
+						<List>
+							{todos.done.map(({id, ...item}) => (
+								<ListItem key={id} item={item} render={renderDoneItem}/>
+							))}
+						</List>
+					)}
 
-          <p>Done: {0}</p>
-        </div>
-      </div>
-    </div>
-  );
+					<p>Done: {0}</p>
+				</Col>
+			</Row>
+		</Container>
+	);
 };
 
 // hoc
 const withName = WrappedComponent => {
-  const name = 'React';
-  const hocComponent = ({ ...props }) => (
-    <WrappedComponent {...props} name={name} />
-  );
+	const name = 'React';
+	const hocComponent = ({...props}) => (
+		<WrappedComponent {...props} name={name}/>
+	);
 
-  return hocComponent;
+	return hocComponent;
 };
 
 // custom hook
 const useTodos = () => {
-  const [todos, setTodos] = useState([]);
+	const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      const list = await fetch('/todos.json').then(res => res.json());
+	useEffect(() => {
+		(async () => {
+			const list = await fetch('/todos.json').then(res => res.json());
 
-      setTodos(list);
-    })();
-  }, []);
+			setTodos(list);
+		})();
+	}, []);
 
-  return todos;
+	return todos;
 };
 
 const mapStateToProps = state => ({
-  fetching: state.fetching,
-  todos: state.todos,
+	fetching: state.fetching,
+	todos: state.todos,
 });
 
 // const mapDispatchToProps = dispatch => ({
@@ -176,15 +178,15 @@ const mapStateToProps = state => ({
 // });
 
 const mapDispatchToProps = dispatch => {
-  const { startLoad, setTodos, endLoad } = bindActionCreators(
-    actions,
-    dispatch
-  );
+	const {startLoad, setTodos, endLoad} = bindActionCreators(
+		actions,
+		dispatch
+	);
 
-  return {
-    startLoad,
-    endLoad,
-    setTodos: list => setTodos(list),
-  };
+	return {
+		startLoad,
+		endLoad,
+		setTodos: list => setTodos(list),
+	};
 };
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.scss'
 
 import reducer from "./reducer";
 import App from "./App";
